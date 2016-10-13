708ae87d21d6c27e0d169b318c807c4284e299d6
chrisonntag <sonntagc@fim.uni-passau.de>
2016-10-13 14:56:04 +0200

    Add check for Microsoft Edge

--- checkIEVersion.js
+++ checkIEVersion.js
@@ -12,10 +12,14 @@ $(document).ready({
 
 	  if (index > 0) {
 	    // return IE version
-	    return parseInt(agent.substring(index+ 5, agent.indexOf(".", index)));
+	    return parseInt(agent.substring(index + 5, agent.indexOf(".", index)));
 	  } else if (agent.indexOf("Trident/7.0") > 0) {
 	    //IE11 wants to trick user agent parsers
 	    return 11;
+	  } else if(agent.indexOf("Edge/")) {
+	  	//return Edge version
+	  	var index = agent.indexOf("Edge/");
+		return parseInt(agent.substring(index + 5, agent.indexOf(".", index)));
 	  } else {
 	    //no IE
 	    return 0;
