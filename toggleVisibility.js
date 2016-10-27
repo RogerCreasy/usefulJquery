5d87230cab49ec79f3064630bae10708841ea784
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2016-10-27 16:48:08 +0530

    adding togglevisibility.js

---
+++ toggleVisibility.js
@@ -0,0 +1,13 @@
+/**
+ * Name; Toggle Visibility
+ * Author: Prakhar Srivastava
+ * Author Email: srivastavaprakhar1@gmail.com
+ * Created : 10/27/16
+ * Description: toogles visibility of any element
+ * example: https://jsfiddle.net/0keL32h3/
+ */
+
+
+$(".element").on("click", function(){
+  $(".elementToFade").fadeToggle("slow", "linear");
+});
